23bb58ec3a8b4eac5392d95bac0d32aebec9ff36
Riley-Robinson <rrtoinfinity@gmail.com>
2020-03-09 18:48:06 -0400

    finnished

--- js/index.js
+++ js/index.js
@@ -40,3 +40,116 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const nav = document.querySelector('nav');
+
+let nav1 = document.querySelectorAll('a');
+
+nav1[0].textContent = siteContent["nav"]["nav-item-1"];
+nav1[1].textContent = siteContent["nav"]["nav-item-2"];
+nav1[2].textContent = siteContent["nav"]["nav-item-3"];
+nav1[3].textContent = siteContent["nav"]["nav-item-4"];
+nav1[4].textContent = siteContent["nav"]["nav-item-5"];
+nav1[5].textContent = siteContent["nav"]["nav-item-6"];
+
+nav1.forEach((text1,i) => (nav1[i].style.color = "green"))
+
+
+let cta = document.querySelector('h1');
+cta.textContent = siteContent["cta"]["h1"];
+
+let cta1 = document.querySelector('button');
+cta1.textContent = siteContent["cta"]["button"];
+
+let codeimg = document.getElementById("cta-img");
+codeimg.setAttribute('src', siteContent["cta"]["img-src"])
+
+
+
+let mainContent = document.querySelectorAll('h4');
+mainContent[0].textContent= siteContent["main-content"]["features-h4"];
+mainContent[1].textContent= siteContent["main-content"]["about-h4"];
+mainContent[2].textContent= siteContent["main-content"]["services-h4"];
+mainContent[3].textContent= siteContent["main-content"]["product-h4"];
+mainContent[4].textContent= siteContent["main-content"]["vision-h4"];
+
+
+let mainContent1 = document.querySelectorAll('p');
+mainContent1[0].textContent= siteContent["main-content"]["features-content"];
+mainContent1[1].textContent= siteContent["main-content"]["about-content"];
+mainContent1[2].textContent= siteContent["main-content"]["services-content"];
+mainContent1[3].textContent= siteContent["main-content"]["product-content"];
+mainContent1[4].textContent= siteContent["main-content"]["vision-content"];
+
+let mainimg = document.getElementById("middle-img");
+mainimg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+
+const contact = document.querySelector(".contact h4");
+contact.textContent= siteContent["contact"]["contact-h4"];
+console.log(contact);
+
+let contact1 = document.querySelectorAll(".contact p");
+contact1[0].textContent= siteContent["contact"]["address"];
+contact1[1].textContent= siteContent["contact"]["phone"];
+contact1[2].textContent= siteContent["contact"]["email"];
+
+let footer1 = document.querySelector("footer p");
+footer1.textContent = siteContent["footer"]["copyright"];
+
+const hr = document.createElement('a');
+hr.textContent= "Human Resource";
+hr.style.color= 'green';
+nav.prepend(hr);
+
+const code = document.createElement('a');
+code.textContent= 'Codeing School';
+code.style.color='green';
+nav.append(code);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let mainContent2 = document.querySelector('p');
+// mainContent2.textContent = siteContent["main-content"]["features-content"];
+
+// let mainContent3 = document.querySelector('h4');
+// mainContent3.textContent = siteContent["main-content"]["about-h4"];
+
+
+//content to get same results as showing nav items
+// const navitem3 = document.querySelectorAll('a:nth-of-type(3)');
+// navitem3.forEach( element => {
+//   element.textContent = "Vision"; 
+// });
+
+// const navitem4 = document.querySelectorAll('a:nth-of-type(4)');
+// navitem4.forEach( element => {
+//   element.textContent = "Features"; 
+// });
+
+// const navitem5 = document.querySelectorAll('a:nth-of-type(5)');
+// navitem5.forEach( element => {
+//   element.textContent = "About"; 
+// });
+
+// const navitem6 = document.querySelectorAll('a:nth-of-type(6)');
+// navitem5.forEach( element => {
+//   element.textContent = "Contact"; 
+// });
+
+// let nav = querySelectorAll('a');
